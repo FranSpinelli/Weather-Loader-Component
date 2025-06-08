9b3157a1c6d32ea885062dd6cf5d76ae51d61451
franciscos <franciscos@pagotic.com>
2025-06-08 13:38:24 -0300

    Proyect Init

---
+++ src/test/java/ar/edu/unq/weatherloadercomponent/WeatherLoaderComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.weatherloadercomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherLoaderComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
