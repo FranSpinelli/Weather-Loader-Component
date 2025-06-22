709283b400bc3729a06709d92ad4b6b739589221
PabloGMarrero <pablo.g.marrero@gmail.com>
2025-06-22 15:57:58 -0300

    feature: sanitizer url apikey value

--- src/main/java/ar/edu/unq/weather_loader_component/infrastructure/persistence/WeatherReportMongoRepository.java
+++ src/main/java/ar/edu/unq/weather_loader_component/infrastructure/persistence/WeatherReportMongoRepository.java
@@ -1,6 +1,5 @@
 package ar.edu.unq.weather_loader_component.infrastructure.persistence;
 
-import ar.edu.unq.weather_loader_component.domain.model.WeatherReport;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
